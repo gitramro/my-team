2e2084ea9b4af8d7cb25a64f7e64e813b0fe99f2
Ramon <ramon.osc93@gmail.com>
2018-11-03 15:29:32 -0700

    matches and firebase

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app'
+import 'firebase/app'
+import 'firebase/database'
+
+var config = {
+  apiKey: "AIzaSyDSzsJ-yx9ju8XgUyPIXWTugpa01hcZYMs",
+  authDomain: "the-team-21d47.firebaseapp.com",
+  databaseURL: "https://the-team-21d47.firebaseio.com",
+  projectId: "the-team-21d47",
+  storageBucket: "the-team-21d47.appspot.com",
+  messagingSenderId: "327584816244"
+};
+
+
+firebase.initializeApp(config);
+
+const firebaseDB = firebase.database();
+const firebaseMatches = firebaseDB.ref('matches');
+
+export {
+    firebase,
+    firebaseMatches
+}
