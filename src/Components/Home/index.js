ad07597bb411406196c978747a746865f99d0480
Ramon <ramon.osc93@gmail.com>
2018-11-04 17:25:44 -0800

    meet the players home and promotion w firebase

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Featured from './Featured';
 import MatchesHome from './Matches';
+import MeetPlayers from './MeetPlayers';
+import Promotion from './Promotion';
 
 const Home = () => {
     return (
         <div className="bck_blue">
             <Featured/>
             <MatchesHome/>
+            <MeetPlayers/>
+            <Promotion/>
         </div>
     );
 };
--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,20 @@
-import firebase from 'firebase/app'
-import 'firebase/app'
-import 'firebase/database'
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/database';
 
 var config = {
-  apiKey: "AIzaSyDSzsJ-yx9ju8XgUyPIXWTugpa01hcZYMs",
-  authDomain: "the-team-21d47.firebaseapp.com",
-  databaseURL: "https://the-team-21d47.firebaseio.com",
-  projectId: "the-team-21d47",
-  storageBucket: "the-team-21d47.appspot.com",
-  messagingSenderId: "327584816244"
+  apiKey: 'AIzaSyDSzsJ-yx9ju8XgUyPIXWTugpa01hcZYMs',
+  authDomain: 'the-team-21d47.firebaseapp.com',
+  databaseURL: 'https://the-team-21d47.firebaseio.com',
+  projectId: 'the-team-21d47',
+  storageBucket: 'the-team-21d47.appspot.com',
+  messagingSenderId: '327584816244'
 };
-
 
 firebase.initializeApp(config);
 
 const firebaseDB = firebase.database();
 const firebaseMatches = firebaseDB.ref('matches');
+const firebasePromotions = firebaseDB.ref('promotions');
 
-export {
-    firebase,
-    firebaseMatches
-}
+export { firebase, firebaseMatches, firebasePromotions };
